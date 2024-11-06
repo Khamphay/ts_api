4cffb756926e1ffd02a6500014d26971a1f23db5
Khamphay <txammuas1@gmail.com>
2024-11-06 23:11:28 +0700

    init

---
+++ src/routes/index.route.ts
@@ -0,0 +1,24 @@
+import { Routes } from '@/interfaces/routes.interface';
+import { Request, Response, Router } from 'express';
+import UserRoute from './users.route';
+import AuthRoute from './auth.route';
+
+export default class IndexRoute implements Routes {
+  public path = '/';
+  public router = Router();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.use('/users', new UserRoute().router);
+    this.router.use('/auth', new AuthRoute().router);
+
+    this.router.get(`${this.path}`, this.index);
+  }
+
+  private index = (req: Request, res: Response) => {
+    res.json({ version: '1.0.0', message: 'Hello World!' });
+  };
+}
